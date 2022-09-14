cea88f65324886b02309b18b750dad355ecd9025
worldmozara <worldmozara@gmail.com>
2022-09-14 10:06:28 +0800

    update test.

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 from wavy.generator import (
-    sine_wave, triangle_wave, square_wave, sawtooth_wave
+    sine_wave, triangle_wave, square_wave, sawtooth_wave, noise_wave
 )
 from wavy.binfmt import duplicate, Encoder
 import wave
 
 
-for f in (sine_wave, triangle_wave, square_wave, sawtooth_wave):
+for f in (sine_wave, triangle_wave, square_wave, sawtooth_wave, noise_wave):
     d = f(261, 1, 44100)
     with wave.open(f"{f.__name__}.wav", "w") as fi:
         fi.setnchannels(2)
